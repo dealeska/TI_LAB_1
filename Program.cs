43917505025b063b2e4e12c9e592fc4694b47658
Alesya Deschenya <69769972+dealeska@users.noreply.github.com>
2021-03-09 01:57:22 +0300

    Add files via upload
    Сделан шифратор "Железнодорожной изгороди".

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI_Lab_1
+{
+    class Program
+    {
+        private static string RailFence_Encrypt(string plaintext, Int32 key)
+        {
+            string ciphertext = "";
+            Int32 max_offset = 2 * (key - 1);
+            Int32 row;
+
+            for (Int32 i = 1; i <= key; i++)
+            {
+                for (Int32 j = 0; j < plaintext.Length; j++)
+                {
+                    row = (j + 1) % max_offset;
+
+                    if (row == 0)
+                        row = max_offset;
+                    if (row > key)
+                        row = key - (row - key);                   
+                    if (row == i)
+                        ciphertext += plaintext[j];
+                }
+            }
+            return ciphertext;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RailFence_Encrypt("SOME MESSAGE", 4));
+        }
+    }
+}
