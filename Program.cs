32f0ae7f8e11b2feace65bcfae9a25ade6e9c41f
Alesya Deschenya <69769972+dealeska@users.noreply.github.com>
2021-04-01 01:19:28 +0300

    Add files via upload
    Добавлен шифратор и дешифратор столбцового метода.

--- Program.cs
+++ Program.cs
@@ -85,15 +85,14 @@ namespace TI_Lab_1
                     ciphertext += (char)((plaintext[i] - 'a' + key) % 26 + 'a');
                 else if (plaintext[i] >= 'A' && plaintext[i] <= 'Z')
                     ciphertext += (char)((plaintext[i] - 'A' + key) % 26 + 'A');
-                else if (plaintext[i] >= 'а' && plaintext[i] <= 'я')
+                else if (plaintext[i] >= 'а' && plaintext[i] <= 'ё')
                     ciphertext += (char)((plaintext[i] - 'а' + key) % 33 + 'а');
-                else if (plaintext[i] >= 'А' && plaintext[i] <= 'Я')
+                else if (plaintext[i] >= 'А' && plaintext[i] <= 'Ё')
                     ciphertext += (char)((plaintext[i] - 'А' + key) % 33 + 'А');
                 else ciphertext += (char)(plaintext[i] + key);
             }
             return ciphertext;
         }
-
         public static string Caesar_Decrypt(string ciphertext, Int32 key)
         {
             string plaintext = "";
@@ -104,17 +103,119 @@ namespace TI_Lab_1
                     plaintext += (char)((ciphertext[i] - 'a' + 26 - key) % 26 + 'a');
                 else if (ciphertext[i] >= 'A' && ciphertext[i] <= 'Z')
                     plaintext += (char)((ciphertext[i] - 'A' + 26 - key) % 26 + 'A');
-                else if (ciphertext[i] >= 'а' && ciphertext[i] <= 'я')
+                else if (ciphertext[i] >= 'а' && ciphertext[i] <= 'ё')
                     plaintext += (char)((ciphertext[i] - 'а' + 33 - key) % 33 + 'а');
-                else if (ciphertext[i] >= 'А' && ciphertext[i] <= 'Я')
-                    plaintext += (char)((ciphertext[i] - 'А' + 26 - key) % 33 + 'А');
+                else if (ciphertext[i] >= 'А' && ciphertext[i] <= 'Ё')
+                    plaintext += (char)((ciphertext[i] - 'А' + 33 - key) % 33 + 'А');
                 else plaintext += (char)(ciphertext[i] - key);
             }
             return plaintext;
         }
 
+        public static string Column_Encrypt(string plaintext, string key)
+        {
+            string ciphertext = "";
+            Int32[] indexKey = new Int32[key.Length];           
+            Int32 i = 0, ind = 0, j = 0, rowNum = 0;
+            if (plaintext.Length % key.Length != 0)
+                rowNum++;
+            rowNum += (plaintext.Length / key.Length);
+            char[,] columnText = new char[rowNum, key.Length];
+
+            for (i = 0; i < key.Length - 1; i++)
+            {
+                for (j = i + 1; j < key.Length; j++)
+                {
+                    if (key[i] <= key[j])
+                        indexKey[j]++;
+                    else
+                        indexKey[i]++;
+                }
+            }
+
+            for (i = 0; i < rowNum; i++)
+            {
+                for (j = 0; j < key.Length && ind < plaintext.Length; j++)
+                {
+                    columnText[i, j] = (char)(plaintext[ind]);
+                    ind++;
+                }
+            }
+
+            ind = 0;
+            for (i = 0; i < key.Length; i++)
+            {
+                j = 0;
+                while (indexKey[j] != ind)
+                    j++;
+
+                for (Int32 k = 0; k < rowNum; k++)
+                {
+                    if (columnText[k, j] != '\0')
+                        ciphertext += columnText[k, j];
+                }
+                ind++;
+            }
+
+            return ciphertext;
+        }
+        public static string Column_Decrypt(string ciphertext, string key)
+        {
+            string plaintext = "";
+            Int32[] indexKey = new Int32[key.Length];
+            Int32 i = 0, j = 0, ind = ciphertext.Length - 1, rowNum = 0;
+            if (ciphertext.Length % key.Length != 0)
+                rowNum++;
+            rowNum += (ciphertext.Length / key.Length);
+            char[,] columnText = new char[rowNum, key.Length];
+
+            for (i = 0; i < key.Length - 1; i++)
+            {
+                for (j = i + 1; j < key.Length; j++)
+                {
+                    if (key[i] <= key[j])
+                        indexKey[j]++;
+                    else
+                        indexKey[i]++;
+                }
+            }
+
+            for (i = key.Length - 1; i >= 0; i--)
+            {
+                j = 0;
+                while (indexKey[j] != i)
+                    j++;
+                if (indexKey[j] == i)
+                {
+                    Int32 row = rowNum;
+                    if ((ciphertext.Length - j - 1) / key.Length != rowNum - 1)
+                        row--;
+
+                    for (Int32 k = row - 1; k >= 0 && ind >= 0; k--)
+                    {
+                        columnText[k, j] = (char)(ciphertext[ind]);
+                        ind--;
+                    }
+                }
+            }
+
+            for (i = 0; i < rowNum; i++)
+            {
+                for (j = 0; j < key.Length; j++)
+                {
+                    if (columnText[i, j] != '\0')
+                        plaintext += columnText[i, j];
+                }
+            }
+
+            return plaintext;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(Column_Encrypt("SOME MESSAGE", "ALESYA"));
+            Console.WriteLine(Column_Decrypt(Column_Encrypt("SOME MESSAGE", "ALESYA"), "ALESYA"));
+
             Console.WriteLine(RailFence_Encrypt("SOME MESSAGE", 4));
             Console.WriteLine(RailFence_Decrypt(RailFence_Encrypt("SOME MESSAGE", 4), 4));
 
